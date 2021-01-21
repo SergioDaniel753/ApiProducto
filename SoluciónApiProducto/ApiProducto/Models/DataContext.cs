7d9ba3e2e60360893c8e2cc87219b72075fe89a2
Sergio Daniel Quevedo <a2019114381@estudiantes.upsa.edu.bo>
2021-01-20 20:41:19 -0400

    Modelo y JWT

---
+++ SoluciónApiProducto/ApiProducto/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProducto.Models
+{
+    using System.Data.Entity;
+    public class DataContext: DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<ApiProducto.Models.Product> Products { get; set; }
+    }
+}
---
+++ SoluciónApiProducto/ApiProducto/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace ApiProducto.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum MarketType
+    {
+        niños=0,
+        jovenes=1,
+        hombres=2,
+        mujeres=3,
+        mayores=4
+    }
+
+    public enum CategoryType
+    {   
+        ropa=0,
+        electrónicos=1,
+        cosméticos=2,
+        mueblería=3,
+        juegos=4,
+        deportes=5,
+        bebidas=6,
+        alimentos=7
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage="Nombre debe contener de 5 a 80 caracteres",MinimumLength=5)]
+        public string Name { get; set; }
+        [Range(0,10000,ErrorMessage="El precio oscila entre 0 a 10000")]
+        public double Price{ get; set; }
+        public MarketType Market { get; set; }
+        public CategoryType Category { get; set; }
+
+    }
+}
